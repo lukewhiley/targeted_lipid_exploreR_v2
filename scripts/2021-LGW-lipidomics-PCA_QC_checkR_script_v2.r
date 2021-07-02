eb857750d264c884102e9117ca539e544fa94dbb
Luke Whiley <luke.whiley@murdoch.edu.au>
2021-07-02 17:48:48 +0800

    workflow update

--- scripts/2021-LGW-lipidomics-PCA_QC_checkR_script_v2.r
+++ scripts/2021-LGW-lipidomics-PCA_QC_checkR_script_v2.r
@@ -16,13 +16,14 @@ pca_p <- lipids_pca_ltr(final_individual_lipid_data, final_class_lipid_data, mul
 saveWidget(pca_p[[1]][[1]], file = paste(project_dir_html, "/", project_name, "_", user_name, "_QC_PCA_all_lipids.html", sep=""))# save plotly widget
 browseURL(paste(project_dir_html, "/", project_name, "_", user_name, "_QC_PCA_all_lipids.html", sep="")) #open plotly widget in internet browser
 saveWidget(pca_p[[2]][[1]], file = paste(project_dir_html, "/", project_name, "_", user_name, "_QC_PCA_lipid_class.html", sep=""))# save plotly widget
-browseURL(paste(project_dir_html, "/", project_name, "_", user_name, "_QC_PCA_lipid_class.html", sep="")) #open plotly widget in internet browser
+#browseURL(paste(project_dir_html, "/", project_name, "_", user_name, "_QC_PCA_lipid_class.html", sep="")) #open plotly widget in internet browser
 
 
-pca_check_status <- dlgInput("Check the PCA plots. Are you happy to continue? or do wish to change the scalling type?", "continue/change")$res
-while(pca_check_status != "continue" & pca_check_status != "change"){
-  pca_check_status <- dlgInput("Check the PCA plots. Are you happy to continue? or do wish to change the scalling type?", "continue/change")$res
-}
+# pca_check_status <- dlgInput("Check the PCA plots. Are you happy to continue? or do wish to change the scalling type?", "continue/change")$res
+# while(pca_check_status != "continue" & pca_check_status != "change"){
+#   pca_check_status <- dlgInput("Check the PCA plots. Are you happy to continue? or do wish to change the scalling type?", "continue/change")$res
+# }
+pca_check_status == "continue"
 }
 
 scale_used <- pca_p[[3]][1]
