7101df472eb49b17d1bbe6e1dfbb7b754505baeb
Luke Whiley <luke.whiley@murdoch.edu.au>
2021-07-02 23:22:33 +0800

    workflow update

--- scripts/2021-LGW-lipidomics-summed_TIC_checkR_script_v2.r
+++ scripts/2021-LGW-lipidomics-summed_TIC_checkR_script_v2.r
@@ -51,8 +51,8 @@ total_summed_tic_removed <- total_summed_tic %>% filter(grepl("removed", removed
 total_summed_tic_pass <- total_summed_tic %>% filter(grepl("pass_qc", removed))
 
 # create a plate list ID
-plate_number <- unique(plateID) %>% substr(14,14) %>% unique()
-plateIDx <- plateIDx <- lapply(unique(plateID), function(FUNC_plateID){
+#plate_number <- unique(total_summed_tic$plateID) %>% substr(14,14) %>% unique()
+plateIDx <- lapply(unique(total_summed_tic$plateID), function(FUNC_plateID){
   #browser()
   grep(FUNC_plateID, total_summed_tic$plateID)[1]}) %>% unlist()
 
